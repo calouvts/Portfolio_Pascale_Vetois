173981e6bf8294d621e6d12041640e15abadd71c
calouvts <pascalevetois@gmail.com>
2021-02-03 21:56:40 +0100

    competences

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,31 +12,23 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-/*    /**
+    /**
      * @Route("/", name="home")
      * @param CompetenceRepository $competenceRepository
      * @return Response
      */
-  /*  public function index(CompetenceRepository $competenceRepository): Response
+    public function index(CompetenceRepository $competenceRepository,
+                          ExperienceRepository $experienceRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'competences' => $competenceRepository->findAll(),
-        ]);
-    }
-**/
-
-    /**
-     * @Route("/", name="home")
-     * @return Response
-     */
-
-    public function indexexperience(ExperienceRepository $experienceRepository): Response
-    {
-        return $this->render('home/index.html.twig', [
             'experiences' => $experienceRepository->findAll(),
         ]);
     }
 
 
-}
+   }
+
+
+
 
