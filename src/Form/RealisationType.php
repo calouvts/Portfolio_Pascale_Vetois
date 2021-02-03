2d4ac37bffefcfeb5f40436124ea365ccc04ac07
calouvts <pascalevetois@gmail.com>
2021-02-04 00:02:14 +0100

    forms

--- src/Form/RealisationType.php
+++ src/Form/RealisationType.php
@@ -2,8 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Competence;
 use App\Entity\Realisation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,37 @@ class RealisationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('realisationphoto')
-            ->add('description')
-            ->add('projectlink')
-            ->add('competences')
+            ->add('name',TextType::class, [
+        'label' => 'Nom',
+        'attr' => [
+            'placeholder' => 'mon site',
+        ],
+    ])
+            ->add('realisationphoto',TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'photo',
+                ],
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'description',
+                'attr' => [
+                    'placeholder' => 'description',
+                ],
+            ])
+            ->add('projectlink',TextType::class, [
+                'label' => 'Lien du projet',
+                'attr' => [
+                    'placeholder' => 'https//',
+                ],
+            ])
+            ->add('competences', EntityType::class, [
+                'class' => Competence::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'by_reference' => false,
+                'expanded' => true
+            ])
         ;
     }
 
