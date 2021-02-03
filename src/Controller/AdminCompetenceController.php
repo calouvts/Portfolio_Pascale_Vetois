212ea9bafb8a08bfca999c2c01828e0984a9b5a5
calouvts <pascalevetois@gmail.com>
2021-02-03 21:34:19 +0100

    competences

---
+++ src/Controller/AdminCompetenceController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Competence;
+use App\Form\CompetenceType;
+use App\Repository\CompetenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+/**
+ * @Route("/admin/competence", name="admin_competence_")
+ */
+
+class AdminCompetenceController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @param CompetenceRepository $competenceRepository
+     * @return Response
+     */
+    public function index(CompetenceRepository $competenceRepository): Response
+    {
+        return $this->render('admin_competence/index.html.twig', [
+            'competences' => $competenceRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/nouveau", name="new")
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $competence = new Competence();
+        $form = $this->createForm(CompetenceType::class, $competence);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($competence);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_competence_index');
+        }
+
+        return $this->render('admin_competence/new.html.twig', [
+            'competence' => $competence,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show(Competence $competence): Response
+    {
+        return $this->render('admin_competence/show.html.twig', [
+            'competence' => $competence,
+        ]);
+    }
+
+    /**
+     * @Route("/modifier/{id}", name="edit")
+     * @ParamConverter("competence", class="App\Entity\Competence", options={"mapping": {"id": "id"}})
+     * @param Request $request
+     * @param Competence $competence
+     * @return Response
+     */
+
+
+    public function edit(Request $request, Competence $competence): Response
+    {
+        $form = $this->createForm(CompetenceType::class, $competence);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_competence_index');
+        }
+
+        return $this->render('admin_competence/edit.html.twig', [
+            'competence' => $competence,
+            'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/delete/{id}", name="delete", methods={"DELETE"})
+     * @return Response
+     */
+
+    public function delete(Request $request, Competence $competence): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$competence->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($competence);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_competence_index');
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Competence;
+use App\Form\CompetenceType;
+use App\Repository\CompetenceRepository;
 use App\Repository\ExperienceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,12 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+/*    /**
+     * @Route("/", name="home")
+     * @param CompetenceRepository $competenceRepository
+     * @return Response
+     */
+  /*  public function index(CompetenceRepository $competenceRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'competences' => $competenceRepository->findAll(),
+        ]);
+    }
+**/
+
     /**
      * @Route("/", name="home")
      * @return Response
      */
 
-    public function index(ExperienceRepository $experienceRepository): Response
+    public function indexexperience(ExperienceRepository $experienceRepository): Response
     {
         return $this->render('home/index.html.twig', [
             'experiences' => $experienceRepository->findAll(),
